205ec2d09dbf52671da6da4c676bea5ae0138e4e
aliensarefake <guoxiaoyu2003429@gmail.com>
2024-02-22 19:06:25 +0800

    remove logos under investment page

--- src/components/investor.tsx
+++ src/components/investor.tsx
@@ -3,34 +3,13 @@ import { Slant } from "./svgs/slant";
 import { Button } from "./ui/button";
 import { paths } from "@/router";
 
-export interface Investor {
-  id: number;
-  logoLink: string;
-  href?: string;
-}
 
-interface InvestorsProps {
-  investors: Investor[];
-}
-
-export default function InvestorView({ investors }: { investors: Investor[] }) {
+export default function InvestorView() {
   return (
     <div className="flex flex-col gap-5 items-center py-4 relative bg-white">
       <Slant className="absolute top-0 w-full translate-y-[-49%] " />
       <p className="text-4xl sm:text-5xl font-bold mb-4 sm:mb-6">Our Thesis</p>
       <div className="grid gap-2 gap-y-8 grid-cols-4 w-[90vw] max-w-2xl">
-        {investors.map((investor, index) => (
-          <div key={investor.id + index} className="flex justify-center">
-            <a className="" href={investor.href ? investor.href : "#"}>
-              <img
-                draggable={false}
-                className="max-w-16 w-full h-auto hover:scale-[1.1] sm:hover:scale-[1.25] transition-all duration-300 ease-in-out"
-                src={investor.logoLink}
-                alt={`${index}`}
-              />
-            </a>
-          </div>
-        ))}
       </div>
       <a className="my-5" href={paths.investments}>
         <Button className="inline-flex items-center text-lg m-4 p-4 h-5">

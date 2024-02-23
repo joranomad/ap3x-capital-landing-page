9f0a3e0c8190a02a941a02e22aaca4855acf8163
aliensarefake <guoxiaoyu2003429@gmail.com>
2024-02-23 15:38:08 +0800

    revise our thesis description

--- src/components/investor.tsx
+++ src/components/investor.tsx
@@ -9,7 +9,23 @@ export default function InvestorView() {
     <div className="flex flex-col gap-5 items-center py-4 relative bg-white">
       <Slant className="absolute top-0 w-full translate-y-[-49%] " />
       <p className="text-4xl sm:text-5xl font-bold mb-4 sm:mb-6">Our Thesis</p>
-      <div className="grid gap-2 gap-y-8 grid-cols-4 w-[90vw] max-w-2xl">
+      <div className="mx-auto lg:max-w-[70%] sm:max-w-[90%] flex flex-wrap mb-6">
+        <div className="flex items-center mx-3 lg:mx-6 mb-6">
+          <img src={"./assets/thesis/secondAI.png"} className="h-10 lg:h-20 mr-2"></img>
+          <div className="text-base lg:text-xl font-bold">d/AI</div>
+        </div>
+        <div className="flex items-center mx-3 lg:mx-6 mb-6">
+          <img src={"./assets/thesis/console.png"} className="h-10 lg:h-20 mr-2"></img>
+          <div className="text-base lg:text-xl font-bold">Gaming</div>
+        </div>
+        <div className="flex items-center mx-3 lg:mx-6 mb-6">
+          <img src={"./assets/thesis/chain.png"} className="h-10 lg:h-20 mr-2"></img>
+          <div className="text-base lg:text-xl font-bold">On-Chain</div>
+        </div>
+        <div className="flex items-center mx-3 lg:mx-6 mb-6">
+          <img src={"./assets/thesis/worldwide.png"} className="h-10 lg:h-20 mr-2"></img>
+          <div className="text-base lg:text-xl font-bold">Ecosystems</div>
+        </div>
       </div>
     </div>
   );

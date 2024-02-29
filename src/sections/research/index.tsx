0f5b07946635f87dac790255a247b5e182663b65
SksOp <shubhaman47@gmail.com>
2024-03-01 00:47:21 +0530

    Refactor research section data structure

--- src/sections/research/index.tsx
+++ src/sections/research/index.tsx
@@ -4,58 +4,6 @@ import SearchBar from "@/components/searchBar";
 import Container from "@/components/ui/container";
 import { cardItems } from "@/constants";
 
-const research: ResearchData[] = [
-  {
-    title: "Lorem Ipsum title ",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    date: "12th June 2021",
-    link: "https://medium.com/",
-  },
-  {
-    title: "Lorem Ipsum title ",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    date: "12th June 2021",
-    link: "https://medium.com/",
-  },
-  {
-    title: "Lorem Ipsum title ",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    date: "12th June 2021",
-    link: "https://medium.com/",
-  },
-  {
-    title: "Lorem Ipsum title ",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    date: "12th June 2021",
-    link: "https://medium.com/",
-  },
-  {
-    title: "Lorem Ipsum title ",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    date: "12th June 2021",
-    link: "https://medium.com/",
-  },
-  {
-    title: "Lorem Ipsum title ",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    date: "12th June 2021",
-    link: "https://medium.com/",
-  },
-  {
-    title: "Lorem Ipsum title ",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    date: "12th June 2021",
-    link: "https://medium.com/",
-  },
-];
-
 function Research() {
   return (
     <Container className="px-4">
